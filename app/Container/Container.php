ba770d5fda95e9fee077b5cfa2958b722242778f
TawsifKarim <tawsif.karim@gmail.com>
2019-11-04 10:30:39 +0600

    gitignore added

--- app/Container/Container.php
+++ app/Container/Container.php
@@ -8,8 +8,23 @@ class Container
 
     public function set($name, callable $closure)
     {
+        $this->items[$name] = $closure;
+    }
+
+    public function get($name)
+    {
+        if($this->has($name)){
+            return $this->items[$name];
+        }
 
     }
 
-
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($this->items[$name]) ? true : false;
+    }
 }
--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 
 
 use App\Container\Container;
+use App\Config\Config;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $container = new Container();
+
+$container->set('config', function(){
+    return new Config();
+});
 
 var_dump($container);
